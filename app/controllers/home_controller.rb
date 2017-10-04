8710d1d50c1ac314da8adaef36b23b0ccf40f7f9
Kyle Geske <stungeye@gmail.com>
2017-10-04 19:22:23 +0000

    The home index action now queries the Pokemon model and the view displays the pokemon count.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,11 @@ class HomeController < ApplicationController
   # The name index isn't special but it's the
   # convention for each controller to have an index.
   def index
+    @pokemon = Pokemon.all
   end
   # After this index action is exectuted the
   # associated view will be automatically load.
   # app/views/home/index.html.erb
+  # All instance variables (@vars) will be
+  # automagically shared with the view.
 end
